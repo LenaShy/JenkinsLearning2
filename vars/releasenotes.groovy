e371e55a7ca97fc0688b4fbb9f198ad58588a9e5
elena.shynkarenko <elena.shynkarenko@gmail.com>
2020-05-09 23:33:47 +0300

    Update releasenotes.groovy

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -5,4 +5,20 @@ def call(Map configure=[:]) {
   def dir = new File(pwd())
   
   println dir
+  
+  def newFile = new File(dir.path + '/releasenotes.txt')
+  newFile.createNewFile()
+  newFile.withWriter('utf-8')
+  {
+    writer ->
+      dir.eachFileRecurse(FileType.ANY) { file ->
+        if (file.isDirectory()) {
+          writer.writeLene(file.name)
+        }
+        else
+        {
+          writer.writeLine('\t' + file.name + '\t' + file.length())
+        }
+      }
+  }
 }
