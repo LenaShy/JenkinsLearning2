c2bfb9c75e44c8ed9d769ceb8f0e915f59b1e683
elena.shynkarenko <elena.shynkarenko@gmail.com>
2020-05-10 00:08:05 +0300

    Update releasenotes.groovy

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -2,6 +2,7 @@ import java.io.*
 import groovy.io.*
 import java.util.Calendar.*
 import java.text.SimpleDateFormat
+import hudson.model.*
 
 @NonCPS
 def call(Map config=[:]) {
@@ -13,7 +14,19 @@ def call(Map config=[:]) {
   def sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
   echo "Date and Time IS: " + sdf.format(date)
   
-  echo "Build Number is: $(BUILD_NUMBER)"
+  //echo "Build Number is: $(BUILD_NUMBER)" do not work
+  
+  def changeLogSets = currentBuild.changeSets;
+  
+  for(change in changeLogSets) {
+    def entries = change.items
+    for(entry in entries) {
+      echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}"
+      for (file in entry.affectedFiles) {
+        echo "${file.editType.name} ${file.path}"
+      }
+    }
+  }
   
   if (config.changes != "false") {
     echo "changes"
